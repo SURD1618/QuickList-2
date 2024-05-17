be20f8bfc5d42d5649862d4666b8912f081804d7
Rohan Das <rd488411@gmail.com>
2024-05-17 18:31:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
 import Header from "./MyComponents/Header";
 import Todos from "./MyComponents/Todos";
 import Footer from "./MyComponents/Footer";
